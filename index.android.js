fcb317cb1557f35a57c28f7475ad6642dd032665
gokulkrishh <krishnangokul9@gmail.com>
2016-06-22 22:50:48 +0530

    Updated main screen

--- index.android.js
+++ index.android.js
@@ -7,13 +7,20 @@
 import React, { Component } from 'react';
 import {
   AppRegistry,
+  TextInput,
   StatusBar,
   StyleSheet,
+  ScrollView,
   Text,
   View
 } from 'react-native';
 
+var Dimensions = require('Dimensions');
+var {width, height} = Dimensions.get('window');
+
 import Spinner from 'react-native-loading-spinner-overlay';
+import NavBar, { NavButton, NavButtonText, NavTitle } from 'react-native-nav';
+import Button from 'react-native-button';
 
 class anonymous extends Component {
   constructor(props) {
@@ -28,26 +35,29 @@ class anonymous extends Component {
       this.setState({
         visible: false
       });
-    }, 2000);
+    }, 1000);
   }
 
   render() {
     const {visible} = this.state;
     return (
       <View style={styles.container}>
-        <StatusBar backgroundColor="rgb(0,150,136)" />
+         <StatusBar style={styles.statusBar} />
         <Spinner visible={visible} color="#fff" overlayColor="rgb(0,150,136)"/>
-        <View visible={visible}>
-          <Text style={styles.welcome}>
-            Welcome to React Native!
-          </Text>
-          <Text style={styles.instructions}>
-            To get started, edit index.ios.js
-          </Text>
-          <Text style={styles.instructions}>
-            Press Cmd+R to reload,{'\n'}
-            Cmd+D or shake for dev menu
-          </Text>
+
+        <NavBar style={styles}>
+          <NavTitle style={styles.title}>Anonymous</NavTitle>
+        </NavBar>
+
+        <View style={{flex: 1, padding: 20}}>
+          <ScrollView>
+            <Text>main</Text>
+          </ScrollView>
+        </View>
+
+        <View>
+          <TextInput autoFocus={true} multiline={true} style={styles.textInput} placeholder={'Type a message...'}/>
+          <Button style={{fontSize: 20, color: 'green'}} styleDisabled={{color: 'red'}}>Send</Button>
         </View>
       </View>
     );
@@ -56,24 +66,42 @@ class anonymous extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#F5FCFF',
+    width: width,
+    height: height,
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+
+  statusBar: {
+    backgroundColor: '#00695c',
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+
+  navBar: {
+    backgroundColor: '#00695c',
   },
+
   spinner: {
-    backgroundColor: "rgb(0,150,136)",
-  }
+    backgroundColor: 'rgb(0,150,136)',
+    padding: 16,
+  },
+
+  title: {
+    fontSize: 17,
+    letterSpacing: 0.5,
+    color: '#fff',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+
+  textInput: {
+    flex: 1,
+    left: 0,
+    right: 10,
+    height: 60,
+    fontSize: 17,
+    lineHeight: 2,
+    padding: 10,
+    backgroundColor: '#ddd'
+  },
 });
 
 AppRegistry.registerComponent('anonymous', () => anonymous);
